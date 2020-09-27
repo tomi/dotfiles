ae77d1304f3737ac7a34b04dee03f4461821065f
Tomi <tomi.turtiainen@gmail.com>
2020-09-27 11:39:16 +0300

    Add initial dot files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dotfiles
-My dotfiles
+
+My dotfiles and computer setup. Based on https://github.com/mathiasbynens/dotfiles
---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+
+brew install azure-cli
+brew install csvkit
+brew install jq
+brew install scmpuff
+brew install tfenv
+brew install tldr
+brew install zsh
+
+# Remove outdated versions from the cellar.
+brew cleanup
